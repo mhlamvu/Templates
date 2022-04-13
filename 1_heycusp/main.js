b79075a24dddff141ba5a6a58c2094dc96a5857b
mhlamvu <bafana.mhlamvu@gmail.com>
2022-04-13 13:17:06 +0200

    gsap animation tuning

--- 1_heycusp/main.js
+++ 1_heycusp/main.js
@@ -11,51 +11,51 @@ anime.timeline().add({
   opacity: [0, 1],
   easing: "easeOutExpo",
   duration: 2000,
-  delay: (el, i) => 4800 + 40 * i
+  delay: (el, i) => 3000 + 40 * i
 })
 
-gsap.to(".box", 2.4, {
-  y: "-100%",
+gsap.to(".box", .5, {
+  y: "-50%",
   ease: Expo.easeInOut,
-  delay: 1
+  delay: 0
 })
 
-gsap.from("img", 4, {
+gsap.from("img", 1, {
   scale: "2",
   ease: Expo.easeInOut,
   delay: 0
 })
 
-gsap.to(".wrapper-image", 2.4, {
+gsap.to(".wrapper-image", 2, {
   width: "400",
   height: "500",
   ease: Expo.easeInOut,
-  delay: 3.6
+  delay: 1
 })
 
 gsap.from(".image", 0.4, {
   opacity: 0,
   ease: Expo.easeInOut,
-  delay: 3.4
+  delay: 2
 })
 
 gsap.to(".left", 2, {
-  x: "-400",
-  rotation: -10,
+  x: "-270",
+  rotation: -12,
   ease: Expo.easeInOut,
-  delay: 3.8
+  delay: 1.3
 })
 
 gsap.to(".right", 2, {
-  x: "100",
+  x: "250",
   rotation: 10,
   ease: Expo.easeInOut,
-  delay: 3.8
+  delay: 1.3
 })
 
 gsap.staggerFrom(".menu > div, .hero-container > div", 2, {
   opacity: 0,
   y: 30,
   ease: Expo.easeInOut,
-  delay: 4.2
+  delay: 1.5
 }, 0.1)
